aa21e5678d805d7b51df11579577be0b706b096e
Jorge-Carrillo8456 <cuentaformaljorgecch@gmail.com>
2026-02-21 08:38:51 -0600

    Implement exercise 6 in ej6.c
    Added implementation for exercise 6 including memory address printing and storage explanation.

--- soluciones/ej6.c
+++ soluciones/ej6.c
@@ -1,6 +1,19 @@
-
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 6
-    return 0;
+#include <stdio.h> 
+int main() {
+//Declara:
+//int m[2][4];
+    int m[2][3];
+//1. Imprime direcciones de todos los elementos.
+    for(int i = 0; i < 2; i++ ){
+        for(int j = 0; j < 3; j++){
+            printf("[%x]", &m[i][j]);
+        }
+        printf("\n");
+    }
+    return 0; 
 }
+//2. Determina si almacenamiento es por filas o columnas.
+//El almacenamiento es por filas, por que primero se llena toda la fila i, luego se llena la fila j
+//3. Deduce fórmula de dirección de m[i][j].
+//Direccion = Base + [ i × C + j ] × F
+//Con "C" siendo el numero de columnas del arreglo y F el numero de filas del arreglo
