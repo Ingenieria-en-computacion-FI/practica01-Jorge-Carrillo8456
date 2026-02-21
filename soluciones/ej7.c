db18781fc9e35a435fb1955e837e8eab6fb17f04
Jorge-Carrillo8456 <cuentaformaljorgecch@gmail.com>
2026-02-21 08:39:22 -0600

    Document memory address comparison in ej7.c
    Added comments explaining memory address comparison in a 2D array.

--- soluciones/ej7.c
+++ soluciones/ej7.c
@@ -1,6 +1,10 @@
-
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 7
-    return 0;
-}
+//Sin ejecutar determina cuál dirección es mayor:
+//&m[0][3]
+//&m[1][0]
+//La dirección mayor es &m[1][0], ya que primero se llenan los elementos horizontales, una vez que estos acaban, 
+//se cambia de fila y asi sucesivamente, esto es asi por el orden de llenado de los ciclos for
+//Justifica matemáticamente.
+//El llenado de los espacios en los arreglos consiste en tomar la primera fila y luegi llenar todas las columnas de esta, hasta que esta
+//termina, se salta a la siguiente fila y asi sucesivamente, por lo que al &m[1][0] tener un valor mas grande de filas, implica 
+//que es su dirección posee un valor mas grande que &m[0][3], ya que 0 < 1 y por consiguiente, la asignacion de ese espacio en la memoria RAM
+//es sucesor del que se asigno para &m[0][3]
